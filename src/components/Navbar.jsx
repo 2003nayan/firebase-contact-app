c15da56a94c8542cb8a07530c94be276fc4c091a
Nayan Katiyara <120472927+2003nayan@users.noreply.github.com>
2024-02-18 20:16:57 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <div className='h-[60px] bg-white my-2 rounded-lg flex items-center justify-center gap-2 text-xl font-medium'>
         <img src="/Vector.svg" alt="" />
-        <h1>Firebase Contact App</h1>
+        <h1>NK's Contact Application</h1>
     </div>
   )
 }
